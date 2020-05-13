262b0d8cb71cd3618d90a485cf5d2b08cce5c91c
Rolf Rander Næss <rolfn@pvv.org>
2020-05-13 20:41:34 +0200

    geotagging of pf-sense filterlog

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import log4js from 'log4js'
 import net from 'net';
 import ngeohash from 'ngeohash'
 
-import parser from './parser'
+import {parseFilterlog,parseAuthFailedMessage} from './parser'
 import doApiCall from './api';
 
 let logger = log4js.getLogger();
@@ -38,38 +38,10 @@ server.on('connection', (socket) => {
 
 		logger.debug('Received data', data.toString())
 
-		const {ip, port, username} = parser(data.toString())
-		logger.debug(`Parsed ${username} ${ip} ${port}`)
-
-		const ipLocation = await doApiCall(ip);
-
-		if(!ipLocation) {
-			logger.error('No data retrieved, cannot continue')
-			return
+		var done = await receiveFilterlog(data.toString())
+		if(!done) {
+			done = await receiveSshLog(data.toString())
 		}
-
-		const geohashed = ngeohash.encode(ipLocation.lat, ipLocation.lon);
-		logger.debug(`Geohashing with lat: ${ipLocation.lat}, lon: ${ipLocation.lon}: ${geohashed}`)
-
-		// Remove lon and lat from tags
-		const {lon, lat, ...others} = ipLocation;
-
-		influx.writePoints([
-			{
-				measurement: 'geossh',
-				fields: {
-					value: 1
-				},
-				tags: {
-					geohash: geohashed,
-					username,
-					port,
-					ip,
-					location: `${ipLocation.regionName}, ${ipLocation.city}`,
-					...others
-				}
-			}
-		]);
 	})
 
 	socket.on('close', () => {
@@ -80,3 +52,94 @@ server.on('connection', (socket) => {
 server.listen(port, () => {
 	logger.info(`TCP Server is running on port ${port}.`);
 });
+
+async function receiveSshLog(data) {
+	const sshmsg = parseAuthFailedMessage(data)
+	if(!sshmsg) {
+		logger.debug("no match for ssh log");
+		return false;
+	}
+	
+	logger.debug(`Parsed ${sshmsg.username} ${sshmsg.ip} ${sshmsg.port}`)
+
+	const ipLocation = await doApiCall(sshmsg.ip);
+
+	if(!ipLocation) {
+		logger.error('No data retrieved, cannot continue')
+		return true;
+	}
+
+	const geohashed = ngeohash.encode(ipLocation.lat, ipLocation.lon);
+	logger.debug(`Geohashing with lat: ${ipLocation.lat}, lon: ${ipLocation.lon}: ${geohashed}`)
+
+	// Remove lon and lat from tags
+	const {lon, lat, ...others} = ipLocation;
+
+	influx.writePoints([
+		{
+			measurement: 'geossh',
+			fields: {
+				value: 1
+			},
+			tags: {
+				geohash: geohashed,
+				username: sshmsg.username,
+				port: sshmsg.port,
+				ip: sshmsg.ip,
+				location: `${ipLocation.regionName}, ${ipLocation.city}`,
+				...others
+			}
+		}
+	]);
+
+	return true;
+}
+
+async function receiveFilterlog(data) {
+	const filterlog = parseFilterlog(data)
+	if(!filterlog) {
+		logger.debug("no match for filterlog log");
+		return false;
+	}
+
+	if(filterlog.ip.startsWith("192.168.")) {
+		logger.debug(`ignore private address: ${filterlog.ip}`);
+		return true;
+	}
+	
+	logger.debug(`Parsed filterlog ${filterlog.ip} ${filterlog.port}`)
+
+	const ipLocation = await doApiCall(filterlog.ip);
+
+	if(!ipLocation) {
+		logger.error('No data retrieved, cannot continue')
+		return true;
+	}
+
+	const geohashed = ngeohash.encode(ipLocation.lat, ipLocation.lon);
+	logger.debug(`Geohashing with lat: ${ipLocation.lat}, lon: ${ipLocation.lon}: ${geohashed}`)
+
+	// Remove lon and lat from tags
+	const {lon, lat, ...others} = ipLocation;
+
+	influx.writePoints([
+		{
+			measurement: 'filterlog',
+			fields: {
+				value: 1
+			},
+			tags: {
+				geohash: geohashed,
+				action: filterlog.action,
+				port: filterlog.port,
+				ip: filterlog.ip,
+				proto: filterlog.proto,
+				location: `${ipLocation.regionName}, ${ipLocation.city}`,
+				...others
+			}
+		}
+	]);
+
+	return true;
+}
+
--- src/parser.js
+++ src/parser.js
@@ -15,8 +15,17 @@
  * @returns {AuthMessageEntry}
  */
 const parseAuthFailedMessage = (message) => {
-    const regex = /\w+ \w+ \w+ ?\w+ ?\w+ (?<username>\w+) \w+ (?<ip>.*) \w+ (?<port>\w+) \w+/;
+    const regex = /Failed password for( invalid user)? (?<username>[^ ]+) from (?<ip>[^ ]+) port (?<port>\d*)/;
     return regex.exec(message)?.groups;
 }
 
-export default parseAuthFailedMessage;
+/**
+ * Parse pf-sense filterlog.
+ * https://docs.netgate.com/pfsense/en/latest/monitoring/raw-filter-log-format.html
+ */
+const parseFilterlog = (message) => {
+    const regex = /(?<ruleno>[0-9]*),(?<subruleno>[0-9]*),(?<anchor>[a-zA-Z0-9]*),(?<tracker>[0-9]*),(?<ifc>[a-zA-Z0-9.]*),(?<reason>[a-zA-Z0-9]*),(?<action>[a-z]*),(?<direction>[a-z]*),((4,(?<tos>[0-9a-fx]*),,(?<ttl>[0-9]*),(?<id>[0-9]*),(?<offset>[0-9]*),(?<flags>[0-9a-zA-Z]*))|(6,(?<class>[0-9]*),(?<flowlabel>[^,]*),(?<hoplimit>[0-9]*))),(?<proto>[0-9]*),(?<protocol>[a-z]*),(?<length>[0-9]*),(?<ip>[0-9a-f.:]*),(?<dstip>[0-9a-f.:]*),(((?<srcport>[0-9]*),(?<dstport>[0-9]*))|(?<icmptype>[0-9a-zA-Z]*))/;
+    return regex.exec(message)?.groups;
+}
+
+export { parseAuthFailedMessage, parseFilterlog };
